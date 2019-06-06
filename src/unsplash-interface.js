cd4152be1e4111208b5de6436061441834b84b50
aj ancheta and megan schulte <aj@>
2019-06-06 09:52:18 -0700

    add functionallity to display random background image on each refresh

--- src/unsplash-interface.js
+++ src/unsplash-interface.js
@@ -1,24 +1,21 @@
 import { RandomImage } from './unsplash-service.js';
 
 
-$(document).ready(function() {
 
-  
-  // $('#weatherLocation').click(function() {
-  //   let city = $('#location').val();
-  //   $('#location').val("");
+$(document).ready(function() {
   
     let randomImage = new RandomImage();  // create instance of WeatherService class
     let promise = randomImage.getRandomBackground();  // call the instance method and pass in user input
 
     promise.then(function(response) {
       let body = JSON.parse(response);
-      // let generate = response["urls"]["raw"] = "https://images.unsplash.com/photo-1558603510-cf83e66d31e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjc1MzQ0fQ";
-      console.log(body);
       
-      $('body').css('background-image', `url('${body.urls.raw}')`);
-      // $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
+      $('body').css(
+        {
+          'background-image': `url('${body.urls.raw})`,
+          'background-repeat': 'no-repeat',
+          'background-size': 'cover' })
+      
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.message}`);
     });
--- src/unsplash-service.js
+++ src/unsplash-service.js
@@ -3,7 +3,7 @@ export class RandomImage {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
       let key = process.env.API_KEY;
-      let url = `https://api.unsplash.com/photos/random?client_id=${key}`;
+      let url = `https://api.unsplash.com/photos/random?client_id=${key}&query=wallpapers&orientation=landscape`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
